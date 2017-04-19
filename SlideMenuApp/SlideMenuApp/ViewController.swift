5796b35be374aadb10102e9b6bcd9363bc1a4ab6
mitakeck <mitake.ck@gmail.com>
2017-04-19 22:33:26 +0900

    add login sample

---
+++ SlideMenuApp/SlideMenuApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMenuApp
+//
+//  Created by mitake on 2017/04/18.
+//  Copyright © 2017 mitake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
