ac1a5b26f54f9abb4365875302bae3455882d477
mitakeck <mitake.ck@gmail.com>
2017-05-30 22:23:08 +0900

    add hello cocoa

---
+++ hellococoa/hellococoa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hellococoa
+//
+//  Created by mitake on 2017/05/21.
+//  Copyright © 2017 mitake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ hellococoa/hellococoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hellococoa
+//
+//  Created by mitake on 2017/05/21.
+//  Copyright © 2017 mitake. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
